b97d1955b09edcd61230e07b5d514b89de935766
Antoni Kepinski <xxczaki@pm.me>
2019-03-31 23:21:49 +0200

    Initial commit

---
+++ doczrc.js
@@ -0,0 +1,11 @@
+export default {
+	title: 'PizzaQL Docs',
+	description: 'Documentation for the PizzaQL project',
+	theme: 'docz-theme-default',
+	themeConfig: {
+		colors: {
+			primary: '#8e2e04'
+		}
+	},
+	dest: '/dist'
+};
